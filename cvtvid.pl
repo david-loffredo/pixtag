ef9c4cf309abfd2cc5d3ed8e76cdab2cefa4258b
David Loffredo <loffredo@steptools.com>
2017-03-26 15:53:29 -0400

    now sets the creation date for plex
    Signed-off-by: David Loffredo <loffredo@steptools.com>

--- cvtvid.pl
+++ cvtvid.pl
@@ -45,6 +45,128 @@ PERL_EOF
     exit(0);
 }
 
+# Exiftool refuses to set the quicktime tags used by Plex.  I suspect
+# that this is just an attempt to promote XMP rather than a technical
+# barrier.
+
+# The way around this is to embed QT tags with ffmpeg, using copy
+# codecs to rebuild the mp4 container.  In particular, we set the
+# (c)day tag, which is used by plex to order the videos.  Next, we
+# copy tags that ffmpeg strips (like XMP) using exiftool.
+
+# Use exif lib to extract all of the metadata from the original, then
+# translate the quicktime-specific tags to equivalent XMP ones and
+# embed in the new file.
+
+# Figure out the creation date, set related new tags, and return the
+# creation date value as an ISO string along with the source.
+sub SetCreateDate {
+    my ($et, $et_orig, $filename) = @_;
+
+    my $createdate = $et_orig-> GetValue('ContentCreateDate', 'Raw');
+    my $datesource;
+
+    $datesource = 'ContentCreateDate' if $createdate;
+
+    # We could also look at 'QuickTime:CreateDate' and ModifyDate but
+    # these are done in UTC when coming off of Kristas camera so we
+    # just bag it and use the VID_2017... filename encoded date.
+
+    # Use the filename first because we might have manually set it
+    # from the AVI file date or some other source.  Note that the
+    # files coming from Krista's phone encode the start time for the
+    # video rather than the end date.
+
+    if (not $createdate) {
+	$createdate = "${1}-${2}-${3}T${4}:${5}:${6}" if 
+	    $filename =~ /(\d\d\d\d)(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d)/;
+	
+	$datesource = 'filename' if $createdate;
+    }
+
+    ## Fall back to the embedded metadata if needed
+    ## format is iso 2017-02-19T16:55:55
+    my $origdate = $et-> GetNewValue('XMP:DateTimeOriginal');
+    if ($origdate and not $createdate) {
+	$createdate = $origdate;
+	$datesource = 'XMP::DateTimeOriginal';
+    }
+
+    ## This is a 32bit UTC value, and at least with Kristas phone is
+    ## the time the video finished.  Prefer the start time if possible
+    my $qttime =   $et_orig-> GetValue('CreateDate','Raw');
+    if ($qttime and not $createdate) {
+	my ($sec,$min,$hour,$mday,$mon,$year) = localtime($qttime);	
+	$createdate = sprintf ("%04d-%02d-%02dT%02d:%02d:%02d", 
+			       $year+1900,$mon+1,$mday,$hour,$min,$sec);
+	$datesource = 'QT::CreateDate (UTC)';
+    }
+
+    # Make the Create and Origdate consistent.  Do not worry about the
+    # QuickTime CreateDate and ModifyDate fields because they are UTC
+    # and are the time the video finishes.
+    if ($createdate ne $origdate) {
+	print " ==> set DateTimeOriginal\n";
+	$et-> SetNewValue('XMP:DateTimeOriginal',$createdate);
+    }
+
+    return ($createdate, $datesource);
+}
+
+
+sub fixtags_mp4 {
+    my ($f, $opts) = @_;
+    my $tmpfile = 'TEMP.mp4';
+    my $newfile = 'NEWFILE.mp4';
+    my $origdir = 'original_createdate';
+
+    if (-f $tmpfile) {
+	unlink $tmpfile or die "Could not remove TEMP file";
+    }
+    
+    if (not -d $origdir) {
+	mkdir $origdir or die "Could not create backup dir";
+    }
+
+    print "$f\n";
+    
+    my $et_orig = new Image::ExifTool;
+    $et_orig->ExtractInfo($f);
+
+    my $et = new Image::ExifTool;
+    my $datesource;
+
+    # Do not stomp on all of the quicktime data that is in there.
+    $et->SetNewValuesFromFile($f);
+    
+    my ($createdate, $datesrc) = SetCreateDate($et, $et_orig, $f);
+    if ($datesrc eq 'ContentCreateDate') {
+	print " ==> has ContentCreateDate, SKIPPING\n";
+	return;
+    }
+    die "COULD NOT GET CREATE DATE" if not $createdate;
+
+    print " ==> date $createdate from $datesrc\n";
+    print " ==> set ContentCreateDate\n";
+
+    -f $newfile && do {
+	unlink $tmpfile or die "Could not remove NEWFILE file";
+    };
+
+    # Make new MP4 container with the create date
+    my $cmd = "ffmpeg -hide_banner -loglevel warning -i $f  -c:v copy -c:a copy -movflags +faststart -metadata date=$createdate $tmpfile";
+    system ($cmd) == 0 or die "Problems in FFMPEG, halting";
+
+    # Add the other XMP tags using exiftool
+    # Note that WriteInfo returns nonzero on success, zero on error
+    $et->WriteInfo($tmpfile, $newfile) == 0 and 
+	die "PROBLEMS WRITING metadata to $newfile, halting\n";
+
+    rename $f, "$origdir/$f" or die "Could not rename original";
+    rename $newfile, $f or die "Could not rename replacement file";
+
+    unlink $tmpfile or warn "Could not remove TEMP file";
+}
 
 
 sub transcode_canon_mov {
@@ -62,16 +184,21 @@ sub transcode_canon_mov {
 	mkdir $origdir or die "Could not create backup dir";
     }
     
-    # Use exif lib to extract all of the metadata from the original,
-    # then translate the quicktime-specific tags to equivalent XMP
-    # ones and embed in the new file.
+    my $et_orig = new Image::ExifTool;
+    $et_orig->ExtractInfo($f);
+
     my $et = new Image::ExifTool;
+    my $datesource;
+
+    # Do not stomp on all of the quicktime data that is in there.
     $et->SetNewValuesFromFile($f);
     
+    my ($createdate, $datesrc) = SetCreateDate($et, $et_orig, $f);
+    die "COULD NOT GET CREATE DATE" if not $createdate;
+
     if ($opts->{rename}) {
-	## format is iso 2017-02-19T16:55:55
-	# "-FileName<DateTimeOriginal" -d "%%Y%%m%%d_%%H%%M%%S_dtl.mov" 
-	my $val = $et-> GetNewValue('XMP:DateTimeOriginal');
+	## expect iso format 2017-02-19T16:55:55
+	my $val = $createdate;
 	$val =~ tr/T:-/_/d;  # make T underscore, strip colons, dash
 	$newfile = "${val}_$opts->{tag}.mp4";
     }
@@ -88,7 +215,7 @@ sub transcode_canon_mov {
     # of 16 which is basically original quality.
     
 
-    my $cmd = "ffmpeg -hide_banner -loglevel warning -i $f -c:v libx264 -preset veryslow -crf 16 -profile:v high -level 4.1 -pix_fmt yuvj420p -movflags +faststart -c:a aac -b:a 160k $tmpfile";
+    my $cmd = "ffmpeg -hide_banner -loglevel warning -i $f -c:v libx264 -preset veryslow -crf 16 -profile:v high -level 4.1 -pix_fmt yuvj420p -movflags +faststart -c:a aac -b:a 160k -metadata date=$createdate $tmpfile";
     
     print "Converting $f\n";
     system ($cmd) == 0 or die "Problems in FFMPEG, halting";
@@ -103,7 +230,7 @@ sub transcode_canon_mov {
 	# even be larger.  If we get less than 5% saving, redo with
 	# compressed audio but the original video (no transcode)
 	#
-	$cmd = "ffmpeg -hide_banner -loglevel warning -i $f  -c:v copy -movflags +faststart -c:a aac -c:a aac -b:a 160k $tmpfile";
+	$cmd = "ffmpeg -hide_banner -loglevel warning -i $f  -c:v copy -movflags +faststart -c:a aac -c:a aac -b:a 160k -metadata date=$createdate $tmpfile";
 
 	print sprintf(" ==> minimal savings (%d%%), keeping original video\n", (1 - $ratio) * 100);
 
@@ -123,7 +250,7 @@ sub transcode_canon_mov {
     print sprintf(" ==> $newfile [saved %d%%]\n", (1 - $ratio) * 100);
 
     # done, rename the original
-    rename $f, "original_movies/$f" or die "Could not rename original";
+    rename $f, "$origdir/$f" or die "Could not rename original";
 }
 
 
